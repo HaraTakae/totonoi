98b13f32ad970787eed822ad7e394fdbea63e7cc
HaraTakae <ta.chip.0206@gmail.com>
2023-10-07 03:43:26 +0000

    deviseの追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "399c2698c5c046708c1775db5cdda70f.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
