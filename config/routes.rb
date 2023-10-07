1dc4ab65379a33e5feb2efc3f2c721e8f7d358f8
HaraTakae <ta.chip.0206@gmail.com>
2023-10-07 08:42:18 +0000

    ヘッダー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,4 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'comments/show'
-    get 'comments/index'
-    get 'comments/edit'
-    get 'comments/new'
-  end
-  namespace :admin do
-    get 'comments/show'
-    get 'comments/index'
-    get 'comments/edit'
-  end
-  namespace :public do
-    get 'areas/show'
-  end
-  namespace :public do
-    get 'users/show'
-    get 'users/edit'
-    get 'users/confirm_withdrawal'
-    get 'users/new'
-  end
   devise_for :admin,skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
 }
@@ -26,5 +6,23 @@ Rails.application.routes.draw do
   sessions: "public/sessions",
   registrations: 'public/registrations',
 }
+
+  scope module: :public do
+    root to: 'homes#top'
+    patch 'users/withdrawal' => "users#withdrawal"
+    get 'users/confirm_withdrawal' => "users#confirm_withdrawal"
+    resources :users, only: [:show, :edit, :update]
+    resources :facility_posts, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+    resources :comments, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+    resources :areas, only: [:show]
+  end
+  
+  namespace :admin do
+    get "" => "homes#top"
+    resources :areas, only: [:index, :create, :edit, :update]
+    resources :comments, only: [:index, :show, :edit, :update]
+    resources :facility_posts, only: [:index, :show, :edit, :update]
+    resources :users, only: [:index, :show, :edit, :update]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
