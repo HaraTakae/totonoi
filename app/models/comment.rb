70f118d5dd6fea5536c6ea3208301f19742eb35c
HaraTakae <ta.chip.0206@gmail.com>
2023-10-08 01:05:37 +0000

    モデル追加

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,6 @@
 class Comment < ApplicationRecord
+  belongs_to :facility_post
+  belongs_to :user
+  
+  validates :comment, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
+  has_many :facility_posts
+  has_many :comments, dependent: :destroy
+         
   def active_for_authentication?
     super && (is_deleted == false)
   end
