2ddbb3d455890cb3a8f8d316e2c47b062a031039
HaraTakae <ta.chip.0206@gmail.com>
2023-10-09 06:43:18 +0000

    投稿newページ作成

--- app/controllers/public/facility_posts_controller.rb
+++ app/controllers/public/facility_posts_controller.rb
@@ -1,13 +1,44 @@
 class Public::FacilityPostsController < ApplicationController
   def index
+    @facility_posts = FacilityPost.all
   end
 
   def new
+    @facility_post = FacilityPost.new
+  end
+  
+  def create
+    @facility_post = FacilityPost.new(facility_post_params)
+    if @facility_post.save
+      flash[:notice] = "新規投稿しました。"
+      redirect_to facility_post_path(@facility_post.id)
+    else
+      flash[:notice] = "新規投稿内容に不備があります。"
+      render :new
+    end
   end
 
   def show
+    @facility_post = FacilityPost.find(params[:id])
   end
 
   def edit
+    @facility_post = FacilityPost.find(params[:id])
+  end
+  
+  def update
+    @facility_post = FacilityPost.find(params[:id])
+    if @facility_post.update(facility_post_params)
+      flash[:notice] = "投稿の変更が完了しました。"
+      redirect_to facility_post_path(@facility_post.id)
+    else
+      flash[:notice] = "投稿の変更内容に不備があります。"
+      render :edit
+    end
+  end
+  
+  private
+  def facility_post_params
+    params.require(:facility_post).permit(:image, :name, :introduction, :area_id, :star, :address).merge(user_id:current_user.id)
   end
 end
--- app/models/facility_post.rb
+++ app/models/facility_post.rb
@@ -1,3 +1,13 @@
 class FacilityPost < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  belongs_to :user
+  belongs_to :area
+  
   has_one_attached :image
+  
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :introduction, presence: true
+  # validates :ster, presence: true
 end
