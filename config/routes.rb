43ac02f4dc550902013a14e4cb836754ae2fcea5
HaraTakae <ta.chip.0206@gmail.com>
2023-10-15 04:50:12 +0000

    ゲストログイン追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   # ゲスト
   devise_scope :user do
-    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+    post "public/guest_sign_in", to: "public/sessions#guest_sign_in"
   end
 
   scope module: :public do
