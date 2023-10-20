7cd5fb5191816acb79e7732eb6f7a96eeb9f96db
HaraTakae <ta.chip.0206@gmail.com>
2023-10-20 08:50:29 +0000

    新着投稿順の追加とレイアウト

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,6 @@
 class Public::HomesController < ApplicationController
   def top
-    @facility_posts = FacilityPost.all
+    @facility_posts = FacilityPost.all.order(created_at: :desc)
     @areas = Area.all
     @area_id = params[:area_id]
   end
